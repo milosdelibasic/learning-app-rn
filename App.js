eb3edaf7a8532b676e73224ffb7e80f7a8410cc3
Milos <milosdelibasic97@gmail.com>
2022-01-22 21:14:26 +0100

    Added blue gray color palette

--- App.js
+++ App.js
@@ -1,14 +1,15 @@
 import {View, Text} from 'react-native';
 import React, {useEffect} from 'react';
 import Config from 'react-native-config';
+import {gray50, gray900, grayDark} from './src/config/colors';
 
 const App = () => {
   useEffect(() => {
     console.log('Environment running: ', Config.ENVIRONMENT);
   }, []);
   return (
-    <View>
-      <Text>Test :)</Text>
+    <View style={{backgroundColor: grayDark, flex: 1}}>
+      <Text style={{color: gray50, fontSize: 40}}>Test :)</Text>
     </View>
   );
 };
