998c70d31aed546c7024d897045eb1c27b92eaa8
Milos <milosdelibasic97@gmail.com>
2022-01-22 21:00:48 +0100

    App renamed to HypeLearn

--- ios/HypeLearnTests/HypeLearnTests.m
+++ ios/HypeLearnTests/HypeLearnTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface learningAppTests : XCTestCase
+@interface HypeLearnTests : XCTestCase
 
 @end
 
-@implementation learningAppTests
+@implementation HypeLearnTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
